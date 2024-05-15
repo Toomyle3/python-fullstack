389bbc428c4317e31595363ab356c701e764f6cc
Tommyle38 <lehoangminh9938@gmail.com>
2024-05-15 11:14:42 +0700

    update url

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const api_url = '/choreo-apis/pythonreactfullstack/backend-nwg/rest-api-be2/v1';
+const api_url = 'https://e8e5d9f3-afdd-475d-97e4-23a0480d33dd-dev.e1-us-east-azure.choreoapis.dev/pythonreactfullstack/backend-nwg/rest-api-be2/v1.0';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : api_url,

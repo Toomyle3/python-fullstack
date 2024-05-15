9ae412a835a310d753ce204df15b5d7e0210b802
Tommyle38 <lehoangminh9938@gmail.com>
2024-05-15 11:07:37 +0700

    update

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const api_url = '/choreo-apis/pythonreactfullstack/backend-nwg/rest-api-be2/v1';
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : api_url,
 });
+
 
 api.interceptors.request.use(
   (config) => {
